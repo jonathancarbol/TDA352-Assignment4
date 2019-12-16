15c75b4c535546b4323887bf8052c7dc2c202791
Li Rönning <li.ronning@gmail.com>
2019-12-16 14:19:38 +0100

    createRandomNumber as BigInteger

--- AttackingElGamal/ElGamal.java
+++ AttackingElGamal/ElGamal.java
@@ -10,7 +10,7 @@ public class ElGamal {
   }
 
   public static void main(String[] arg) {
-    String filename = "input.txt";
+    String filename = "AttackingElGamal/input.txt";
     try {
       BufferedReader br = new BufferedReader(new FileReader(filename));
       BigInteger p = new BigInteger(br.readLine().split("=")[1]);
@@ -42,7 +42,33 @@ public class ElGamal {
   public static BigInteger recoverSecret(BigInteger p, BigInteger g,
       BigInteger y, int year, int month, int day, int hour, int minute,
       int second, BigInteger c1, BigInteger c2) {
+
+    BigInteger r = new BigInteger(String.valueOf(createRandomNumber(year,month,day,hour,minute,second)));
+
+
+    // CryptoLib.ModInv();
+
     return c1;
   }
+
+  public static BigInteger createRandomNumber(int year, int month, int day, int hour, int minute, int second){
+    // int x = year*(10^10) + month*(10^8) + day*(10^6) + hour*(10^4) + minute*(10^2) + second;
+
+    BigInteger ye = new BigInteger("10000000000");
+    BigInteger me = new BigInteger("100000000");
+    BigInteger de = new BigInteger("1000000");
+    BigInteger he = new BigInteger("10000");
+    BigInteger mine = new BigInteger("100");
+
+    BigInteger yB = new BigInteger(String.valueOf(year)).multiply(ye);
+    BigInteger mB = new BigInteger(String.valueOf(month)).multiply(me);
+    BigInteger dB = new BigInteger(String.valueOf(day)).multiply(de);
+    BigInteger hB = new BigInteger(String.valueOf(hour)).multiply(he);
+    BigInteger minB = new BigInteger(String.valueOf(minute)).multiply(mine);
+    BigInteger sB = new BigInteger(String.valueOf(second));
+
+    return yB.add(mB).add(dB).add(hB).add(minB).add(sB);
+  }
+
 
 }
